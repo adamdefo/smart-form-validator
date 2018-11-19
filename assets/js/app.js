75ed74dcfbbf87b42d1c58b0708f2bece4bf5a13
aleksey <maylo.kondratev@yandex.ru>
2018-11-20 00:15:28 +0300

    Ajax

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
 var smartFormValidator = new SmartFormValidator('.form-validate');
+
+var url = 'api/ip_adress.php';
+var json = getFormValues('form-feedback');
+
+makeRequest('POST', url, json).then(function (response) {
+    var res = JSON.parse(response);
+}).catch(function (err) {
+    console.error('Упс! Что-то пошло не так.', err.statusText);
+});
